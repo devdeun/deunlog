73045966a6e605a02a8f771b48272140376e5813
deun <devdeun@gmail.com>
2024-08-12 21:59:30 +0900

    🎉

---
+++ README.md
@@ -0,0 +1,1 @@
+# deun blog
---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'deun blog',
+  description: '개발 블로그',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
