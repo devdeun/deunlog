5327846e02f1fe691716e1c0aee54ffaa18e62fc
deun <devdeun@gmail.com>
2025-01-02 18:04:06 +0900

    ✨ Toc 추가 (#12)

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -9,3 +9,46 @@ export const generateDescription = (content: string) => {
 
   return `${parsedContent}...`
 }
+
+// Table of content
+export type TOCSectionModel = TOCSubSectionModel & {
+  subSections: TOCSubSectionModel[]
+}
+
+export type TOCSubSectionModel = {
+  slug: string
+  text: string
+}
+
+export const parseToc = (source: string) => {
+  return source
+    .split('\n')
+    .filter((line) => line.match(/(^#{1,3})\s/))
+    .reduce<TOCSectionModel[]>((acc, rawHeading) => {
+      const newAcc = [...acc]
+      const removeMdx = rawHeading
+        .replace(/^##*\s/, '')
+        .replace(/[*,~]{2,}/g, '')
+        .replace(/(?<=\])\((.*?)\)/g, '')
+        .replace(/(?<!\S)((http)(s?):\/\/|www\.).+?(?=\s)/g, '')
+
+      const section = {
+        slug: removeMdx
+          .trim()
+          .toLowerCase()
+          .replace(/[^a-z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣 -]/g, '')
+          .replace(/\s/g, '-'),
+        text: removeMdx,
+      }
+
+      const isSubTitle = rawHeading.split('#').length - 1 === 3 // h3
+
+      if (acc.length && isSubTitle) {
+        newAcc.at(-1)?.subSections.push(section)
+      } else {
+        newAcc.push({ ...section, subSections: [] })
+      }
+
+      return newAcc
+    }, [])
+}
