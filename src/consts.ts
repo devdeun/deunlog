846aafd60121018c74844e1cb3728842715a646f
deun <devdeun@gmail.com>
2025-01-18 10:37:14 +0900

    ✨ og 이미지 추가 (#32)

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 export const SITE = {
   TITLE: 'deunlog',
   TITLE_TEMPLATE: '%s | deunlog',
-  DESCRIPTION: '',
-  IMAGE: '',
+  DESCRIPTION: '웹 프론트엔드 개발자 deun입니다.',
+  IMAGE: '/images/og.png',
 }
 
 export const NOTES_CATEGORIES = {
