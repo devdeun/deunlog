cf8085e3e0aa3547a89533b8b2c15c3d95f33ed8
deun <devdeun@gmail.com>
2025-02-23 20:45:22 +0900

    ✨ 소개 문구 수정

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,8 @@
 export const SITE = {
   TITLE: 'deunlog',
   TITLE_TEMPLATE: '%s | deunlog',
-  DESCRIPTION: '웹 프론트엔드 개발자 deun입니다.',
+  DESCRIPTION:
+    '웹 프론트엔드 개발자 deun입니다. 다양한 문제를 분석하고 해결책을 찾는 과정에서 큰 성취감을 느낍니다. 문제 해결에 그치지 않고 이를 통해 서비스의 품질을 높여 더 나은 사용자 경험을 제공하고자 노력합니다.',
   IMAGE: '/images/og.png',
 }
 
